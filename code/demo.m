97b4b32ccb160dea893c5ef09375aafa41023ffd
Xyuan13 <31299817+Xyuan13@users.noreply.github.com>
2017-11-02 15:54:59 +0800

    Update demo.m

--- code/demo.m
+++ code/demo.m
@@ -1,26 +1,25 @@
+% Attention
+% This code has been tested successfully on GPU GTX-1080 with 12g memory
+% If your GPU have not enough memory and the code causes an "out of meory"
+% error, your can change the input_dim from 512 to 320 in the protoFile
+
 % matcaffe path
 matcaffePath = '../deeplab-caffe/matlab/';
 addpath(matcaffePath)
 
 % change to your images
 data_root='../data';
-img_id='0_0_775';
+img_id='00000';
 suffix='.jpg';
 
 % set parameters for the CNN model
-models_root='../models_prototxts';
+models_root='../models_prototxts/';
 model_id='VGG';
-
-% If your don't have enough memory of GPU, set the downsample_flag true,
-% and the input data size would be changed from 512 to 320.
-% Since your image size may be larger than the input data size(320/512),
-% please set the suitable ratio to downsalple the image 
-% otherwise you can't get the proper result.
-downsample_flag = true;
-ratio = 0.5;
 
 % GPU / CPU
 useGPU = true;
+
+isSave = true;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 param.protoFile = fullfile(models_root, sprintf('MSRNet-%s-test.prototxt',model_id));
 param.modelFile = fullfile(models_root, sprintf('MSRNet-%s.caffemodel',model_id));
@@ -46,12 +45,15 @@ net = caffe.Net(param.protoFile, param.modelFile, 'test');
 
 im_data=imread(fullfile(data_root,'imgs',[img_id suffix]));
 src=im_data;
-if downsample_flag
-    input=zeros(320,320,3);   
-    im_data=imresize(im_data, ratio);
-else
-    input=zeros(512,512,3);
-    net.blobs('data').reshape([512 512 3 1]);
+
+data_shape=net.blobs('data').shape;
+MAX_LEN=data_shape(1);
+
+input=zeros(MAX_LEN, MAX_LEN, 3);
+
+if max(max(size(im_data))) > MAX_LEN
+  ratio = MAX_LEN / max(max(size(im_data))) ;
+  im_data=imresize(im_data, ratio);
 end
 
 [H, W, ~]=size(im_data);
@@ -65,33 +67,28 @@ input(1:W,1:H,:)=im_data;
 
 net.forward({input});
 output = net.blobs('final_fusion').get_data();
-output_crf= net.blobs('final_fusion_crf').get_data();
 
 smap=output(:,:,2);
 smap=permute(smap,[2,1,3]);
 smap=smap(1:H, 1:W);
 
-smap_crf = output_crf(:,:,2);
-smap_crf = permute(smap_crf,[2,1,3]);
-smap_crf = smap_crf(1:H, 1:W);
 %% show result
-% save the compare result
 h=figure(1);    
 
-subplot(1,3,1);
+subplot(1,2,1);
 imshow(src);
 title('Image');
 
-subplot(1,3,2);
+subplot(1,2,2);
 imshow(smap);
 title('SaliencyMap'); 
 
-subplot(1,3,3);
-imshow(smap_crf);
-title('SaliencyMap+CRF'); 
-
 %% save saliency map
-path=fullfile(data_root,'pre',[img_id '.png']);
-imwrite(smap, path);
-path=fullfile(data_root,'pre',[img_id '_crf.png']);
-imwrite(smap_crf, path);
+if isSave
+  save_dir = fullfile(data_root, 'pre');
+  if ~exist(save_dir,'dir')
+     mkdir(save_dir);
+  end
+  path=fullfile(save_dir,[img_id '.png']);
+  imwrite(smap, path);
+end
